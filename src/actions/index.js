81746647b050a451d96093bc48865b5028e7fd39
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-04-10 13:47:08 +0200

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonplaceholder from '../apis/jsonplaceholder';
+
+export const fetchPosts = async () => {
+  const response = await jsonplaceholder.get('/posts')
+
+  return {
+    type: 'FETCH_POST',
+    payload: response
+  };
+};
+
